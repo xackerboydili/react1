024c88d45d108dee1bdb971b7e46a620832d03ae
xackerboydili <isoqovdilshod47@gmail.com>
2022-12-03 01:28:06 +0500

    Filtering

---
+++ src/list.js
@@ -0,0 +1,10 @@
+export const list = [
+    {id:1, name: 'John'},
+    {id:2, name: 'Rick'},
+    {id:3, name: 'Joey'},
+    {id:4, name: 'Albert'},
+    {id:5, name: 'Ann'},
+    {id:6, name: 'Hardin'},
+    {id:7, name: 'Karl'},
+    {id:8, name: 'Suzy'}
+]
--- src/state.jsx
+++ src/state.jsx
@@ -1,38 +1,55 @@
+import { specialCharMap } from "@testing-library/user-event/dist/keyboard";
 import React from "react";
+import {list} from "./list"
 
-class State extends React.Component{
-
-  constructor(props){
-    super(props)
+class State extends React.Component {
+  constructor(state){
+    super(state);
     this.state = {
-      count: 0,
-      title: 'hi'
-    }
+      data: list,
 
+    }   
   }
-
-
   render(){
-    
-    const plus = () => {
-      if(this.state.count < 10)this.setState({count : this.state.count + 1})}
+    // const onChange = (e) => {
+    //   this.setState({name : e.target.value})
+    // }
 
-    const minus = () => {
-      if(this.state.count > 1)this.setState({count : this.state.count - 1})}
-    
-    const onChange = (event) => {
-      console.log(event.target.value);
-      this.setState({title: event.target.value});
+    // const plus = () => {
+    //   if(this.state.count < Infinity) this.setState({count: this.state.count + 1})
+    // }
+
+    const onFilter = (e) => {
+      let result = list.filter((value) => value.name.includes(e.target.value))
+      this.setState({
+        data: result,
+      })
     }
     return(
+      // <div>
+      //   {/* <input onChange={onChange} type="text" placeholder="" /> */}
+      //   <h1>{this.state.count}</h1>
+
+      //   <button onClick={plus}>+</button>
+      // </div>
       <div>
-        <input onChange={onChange} type="text" placeholder="your list"/>
-        <h1>{this.state.title}</h1>
-        <h1>State {this.state.count}</h1>
-        <button onClick={plus}>+</button>
-        <button onClick={minus}>-</button>
+        <input onChange={onFilter} type="text" placeholder="find" />
+        {this.state.data.map(({id, name}) =>{
+          return(
+            <h1 key={id}>{id} {name}</h1>
+          )
+        })}
       </div>
+      
     )
   }
+
+
 }
+
+
+
+
+
+
 export default State;
